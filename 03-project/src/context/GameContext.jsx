3b3a5b3e9b5cf24e6efa1a08e99d970928aaabc2
Karan <karankumar10986745@gmail.com>
2026-01-15 22:01:57 +0530

    Update gameContext.jsx

--- 03-project/src/context/GameContext.jsx
+++ 03-project/src/context/GameContext.jsx
@@ -1,8 +1,3 @@
 import { createContext } from "react";
 
-export const GameContext = createContext({
-	score: 0,
-	setScore: () => {},
-	selectedNumber: null,
-	setSelectedNumber: () => {},
-});
+export const GameContext = createContext();
